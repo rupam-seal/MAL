4b1515a767e31526bc89bc16efb883ca16d9fb46
rupam-seal <rupam.x.seal@gmail.com>
2022-02-19 18:09:34 +0530

    completed

---
+++ Tut/variables.js
@@ -0,0 +1,17 @@
+// x, y, and z, are variables, declared with the var keyword:
+var x = 5;
+var y = 6;
+var z = x + y;
+
+// x, y, and z, are variables, declared with the let keyword:
+let x = 5;
+let y = 6;
+let z = x + y;
+
+// x, y, and z, are undeclared variables:
+x = 5;
+y = 6;
+z = x + y;
+
+// If you want a general rule: always declare variables with const.
+// If you think the value of the variable can change, use let.
