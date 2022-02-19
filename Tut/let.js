00f7da47f83ced7926805d113b79a6f04e52dfaf
rupam-seal <rupam.x.seal@gmail.com>
2022-02-19 18:09:46 +0530

    completed

---
+++ Tut/let.js
@@ -0,0 +1,11 @@
+// Variables defined with let cannot be redeclared.
+
+// You cannot accidentally redeclare a variable.
+
+// With let you can not do this:
+
+let x = 'John Doe';
+
+let x = 0;
+
+// SyntaxError: 'x' has already been declared
