79e792d371cfe9668073700f534e008fb28c9def
rupam-seal <rupam.x.seal@gmail.com>
2022-02-20 18:11:01 +0530

    completed

---
+++ Tut/operators.js
@@ -0,0 +1,15 @@
+// let x = 10;
+
+// Assign the value x + y to z:
+// let z = x + y;
+// let z = x * y;
+let x = (100 + 50) * a;
+
+// Operator	Example	Same As
+// =	x = y	x = y
+// +=	x += y	x = x + y
+// -=	x -= y	x = x - y
+// *=	x *= y	x = x * y
+// /=	x /= y	x = x / y
+// %=	x %= y	x = x % y
+// **=	x **= y	x = x ** y
