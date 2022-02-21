12f4755ed64a8eee10a0b31b69ca763cf7b0ffa1
rupam-seal <rupam.x.seal@gmail.com>
2022-02-21 18:11:12 +0530

    completed

---
+++ Tut/arithmetic.js
@@ -0,0 +1,9 @@
+// Operator	Description
+// +	Addition
+// -	Subtraction
+// *	Multiplication
+// **	Exponentiation (ES2016)
+// /	Division
+// %	Modulus (Remainder)
+// ++	Increment
+// --	Decrement
