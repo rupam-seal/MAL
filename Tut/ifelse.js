5e724b0862abfe064d6c50d18b6156965fb4d475
rupam-seal <rupam.x.seal@gmail.com>
2022-02-22 18:13:33 +0530

    completed

---
+++ Tut/ifelse.js
@@ -0,0 +1,17 @@
+let hour = 10;
+
+if (hour < 18) {
+  greeting = 'Good day';
+} else {
+  greeting = 'Good evening';
+}
+
+let time = 15;
+
+if (time < 10) {
+  greeting = 'Good morning';
+} else if (time < 20) {
+  greeting = 'Good day';
+} else {
+  greeting = 'Good evening';
+}
