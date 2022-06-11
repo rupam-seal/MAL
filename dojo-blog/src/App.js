3eb1a0fd69a956240cffd4b1cd655ac6b7b87dd8
rupam-seal <rupam.x.seal@gmail.com>
2022-06-11 16:38:34 +0530

    ✨completed: lesson-4

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -3,22 +3,17 @@ import './App.css';
 function App() {
   const title = 'Welcome to the new blog';
   const likes = 50;
-  // const person = { name: 'yoshi', age: 30 };
-  const link = 'http://www.google.com';
-
+  // const person = { name: 'yeshi', age: 50 };
+  const link = 'https://www.google.com';
   return (
     <div className="App">
       <div className="content">
-        <h1>{ title }</h1>
-        <p>Liked { likes } times</p>
-
-        {/* <p>{ person }</p> */}
-
-        <p>{ 10 }</p>
-        <p>{ "hello, ninjas" }</p>
-        <p>{ [1,2,3,4,5] }</p>
-        <p>{ Math.random() * 10 }</p>
-
+        <h1>{title}</h1>
+        <p>Liked {likes} times</p>
+        {/* <p>{person}</p> */}
+        <p>{10}</p>
+        <p>{[1, 2, 3, 4, 5]}</p>
+        <p>{Math.random() / 10}</p>
         <a href={link}>Google Site</a>
       </div>
     </div>
