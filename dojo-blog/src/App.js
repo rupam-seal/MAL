a69bb022fec7922bb913dbff7d21dcc8a173cf65
rupam-seal <rupam.x.seal@gmail.com>
2022-06-11 16:39:17 +0530

    ✨completed: lesson-5

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,20 +1,13 @@
 import './App.css';
+import Home from './Home';
+import Navbar from './Navbar';
 
 function App() {
-  const title = 'Welcome to the new blog';
-  const likes = 50;
-  // const person = { name: 'yeshi', age: 50 };
-  const link = 'https://www.google.com';
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{title}</h1>
-        <p>Liked {likes} times</p>
-        {/* <p>{person}</p> */}
-        <p>{10}</p>
-        <p>{[1, 2, 3, 4, 5]}</p>
-        <p>{Math.random() / 10}</p>
-        <a href={link}>Google Site</a>
+        <Home />
       </div>
     </div>
   );
