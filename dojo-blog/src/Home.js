68c73a7b82c5d830df7a04c45446639f5c4b2eb0
rupam-seal <rupam.x.seal@gmail.com>
2022-06-13 16:40:54 +0530

    ✨completed: lesson-9

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,20 +1,29 @@
 import React, { useState } from 'react';
 
 const Home = () => {
-  const [name, setName] = useState();
+  const blogsData = [
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    {
+      title: 'Web dev',
+      body: 'lorem ipsum...',
+      author: 'mario',
+      id: 3,
+    },
+  ];
 
-  const handleClick = () => {
-    const inp = document.getElementById('inp');
-    setName(inp.value);
-  };
-  return (
-    <div className="home">
-      <h2>Homepage</h2>
-      <input id="inp"></input>
-      <p>{name}</p>
-      <button onClick={handleClick}>Click me</button>
-    </div>
-  );
+  const [blogs, setBlogs] = useState(blogsData);
+
+  const content = blogs.map((blog) => {
+    return (
+      <div className="blog-preview" key={blog.id}>
+        <h2>{blog.title}</h2>
+        <p>Written by {blog.author}</p>
+      </div>
+    );
+  });
+
+  return <div className="home">{content}</div>;
 };
 
 export default Home;
