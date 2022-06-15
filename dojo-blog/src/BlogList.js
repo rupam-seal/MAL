76a98c9aac8bde9c56ed87d7af718d95da6a0e9c
rupam-seal <rupam.x.seal@gmail.com>
2022-06-15 16:42:04 +0530

    ✨completed: lesson-12

--- dojo-blog/src/BlogList.js
+++ dojo-blog/src/BlogList.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
-const BlogList = ({ blogs, title }) => {
-  console.log(blogs);
+const BlogList = ({ blogs, title, handleDelete }) => {
   return (
     <div className="blog-list">
       <h2>{title}</h2>
@@ -10,6 +9,7 @@ const BlogList = ({ blogs, title }) => {
           <div className="blog-preview" key={blog.id}>
             <h2>{blog.title}</h2>
             <p>Written by {blog.author}</p>
+            <button onClick={() => handleDelete(blog.id)}>delete blog</button>
           </div>
         );
       })}
