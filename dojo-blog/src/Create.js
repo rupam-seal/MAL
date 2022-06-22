53f19da84ef2e2e3171e1a1347fd4f9ac63784fe
rupam-seal <rupam.x.seal@gmail.com>
2022-06-22 16:52:20 +0530

    ✨completed: lesson-27

--- dojo-blog/src/Create.js
+++ dojo-blog/src/Create.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Create = () => {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('mario');
+
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
+      <form>
+        <label>Blog title:</label>
+        <input
+          onChange={(e) => setTitle(e.target.value)}
+          type="text"
+          required
+        />
+
+        <label>Blog body:</label>
+        <textarea onChange={(e) => setBody(e.target.value)} required></textarea>
+
+        <label>Blog author:</label>
+        <select
+          value={author}
+          onChange={(e) => {
+            setAuthor(e.target.value);
+          }}
+        >
+          <option value="mario">mario</option>
+          <option value="yoshi">yoshi</option>
+        </select>
+        <button>Add Blog</button>
+      </form>
     </div>
   );
 };
