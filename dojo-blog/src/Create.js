03938580bd2d529ce754f556055f18ef26fe071c
rupam-seal <rupam.x.seal@gmail.com>
2022-06-23 16:55:05 +0530

    ✨completed: lesson-30

--- dojo-blog/src/Create.js
+++ dojo-blog/src/Create.js
@@ -4,6 +4,7 @@ const Create = () => {
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
   const [author, setAuthor] = useState('mario');
+  const [isPending, setIsPending] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -12,7 +13,17 @@ const Create = () => {
       body,
       author,
     };
-    console.log(blog);
+
+    setIsPending(true);
+
+    fetch('http://localhost:8000/blogs', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(blog),
+    }).then(() => {
+      setIsPending(false);
+      console.log('new blog added');
+    });
   };
 
   return (
@@ -39,7 +50,8 @@ const Create = () => {
           <option value="mario">mario</option>
           <option value="yoshi">yoshi</option>
         </select>
-        <button>Add Blog</button>
+        {!isPending && <button>Add Blog</button>}
+        {isPending && <button disabled>Adding blog ...</button>}
       </form>
     </div>
   );
