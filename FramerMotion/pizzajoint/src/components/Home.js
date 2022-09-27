1e1ad96eecb4d908394f08c9e887f00c8afb7884
rupam-seal <rupam.x.seal@gmail.com>
2022-09-27 18:26:01 +0530

    ✨completed Lesson-3

--- FramerMotion/pizzajoint/src/components/Home.js
+++ FramerMotion/pizzajoint/src/components/Home.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 const Home = () => {
   return (
-    <div className="home container">
-      <h2>
+    <motion.div
+      className="home container"
+      animate={{ rotateZ: 360, opacity: 0.2, marginTop: 200 }}
+    >
+      <motion.h2 animate={{ fontSize: 50, color: '#FF2994', x: 100, y: -100 }}>
         Welcome to Pizza Joint
-      </h2>
+      </motion.h2>
       <Link to="/base">
-        <button>
+        <motion.button animate={{ scale: 1.5 }}>
           Create Your Pizza
-        </button>
+        </motion.button>
       </Link>
-    </div>
-  )
-}
+    </motion.div>
+  );
+};
 
 export default Home;
