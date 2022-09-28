e4c241dc09fdc042b1edd5c55a1e3c5e0fd5af3b
rupam-seal <rupam.x.seal@gmail.com>
2022-09-28 18:33:03 +0530

    ✨completed Lesson-4

--- FramerMotion/pizzajoint/src/components/Base.js
+++ FramerMotion/pizzajoint/src/components/Base.js
@@ -1,34 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 const Base = ({ addBase, pizza }) => {
   const bases = ['Classic', 'Thin & Crispy', 'Thick Crust'];
 
   return (
     <div className="base container">
-
       <h3>Step 1: Choose Your Base</h3>
       <ul>
-        {bases.map(base => {
+        {bases.map((base) => {
           let spanClass = pizza.base === base ? 'active' : '';
           return (
             <li key={base} onClick={() => addBase(base)}>
-              <span className={spanClass}>{ base }</span>
+              <span className={spanClass}>{base}</span>
             </li>
-          )
+          );
         })}
       </ul>
 
       {pizza.base && (
         <div className="next">
           <Link to="/toppings">
-            <button>Next</button>
+            <motion.button
+              initial={{ x: '-100vw', opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+            >
+              Next
+            </motion.button>
           </Link>
         </div>
       )}
-
     </div>
-  )
-}
+  );
+};
 
 export default Base;
--- FramerMotion/pizzajoint/src/components/Home.js
+++ FramerMotion/pizzajoint/src/components/Home.js
@@ -6,15 +6,12 @@ const Home = () => {
   return (
     <motion.div
       className="home container"
-      animate={{ rotateZ: 360, opacity: 0.2, marginTop: 200 }}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
     >
-      <motion.h2 animate={{ fontSize: 50, color: '#FF2994', x: 100, y: -100 }}>
-        Welcome to Pizza Joint
-      </motion.h2>
+      <h2 animate={{}}>Welcome to Pizza Joint</h2>
       <Link to="/base">
-        <motion.button animate={{ scale: 1.5 }}>
-          Create Your Pizza
-        </motion.button>
+        <motion.button animate={{}}>Create Your Pizza</motion.button>
       </Link>
     </motion.div>
   );
